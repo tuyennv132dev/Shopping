c6aa78ae72423816deb3993362e6d3da49d28f94
Tuyen Nguyen <jinzaitech.staff12@gmail.com>
2026-05-28 16:16:24 +0700

    Hide all Google Translate UI, only use dropdown ENG/VIE

--- js/google-translate.js
+++ js/google-translate.js
@@ -1,46 +1,55 @@
 /**
  * google-translate.js - Google Translate cho Huyen Tuyen Rice
  * Hỗ trợ: ENG (English) và VIE (Tiếng Việt)
- * Khi chọn ENG: xóa hoàn toàn cookie Google Translate để tắt dịch
- * Khi chọn VIE: set cookie để Google Translate dịch sang tiếng Việt
+ * 
+ * - Chọn VIE: bật Google Translate, ẩn banner Google, chỉ dùng dropdown của dự án
+ * - Chọn ENG: xóa cookie Google Translate, reload về tiếng Anh mặc định
  */
 
 (function() {
   'use strict';
 
-  // ---- 0. Kiểm tra và khôi phục trạng thái ----
+  // ---- 0. Khôi phục trạng thái ----
   var savedLang = 'en';
   try {
     var s = localStorage.getItem('gt_lang');
     if (s === 'vi' || s === 'en') savedLang = s;
   } catch(e) {}
 
-  // Nếu lần trước chọn VI, set cookie để Google Translate dịch
-  // Nếu lần trước chọn EN, XÓA cookie để tắt hoàn toàn Google Translate
+  // Set/Xóa cookie googtrans
   if (savedLang === 'vi') {
     document.cookie = 'googtrans=/en/vi; path=/;';
   } else {
-    // Xóa cookie googtrans để tắt Google Translate
     document.cookie = 'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
   }
 
-  // ---- 1. Thêm div ẩn + script Google Translate ----
-  function init() {
-    // Div ẩn cho Google Translate
+  // ---- CSS ẩn hoàn toàn Google Translate ----
+  var style = document.createElement('style');
+  style.textContent = 
+    '.goog-te-banner-frame.skiptranslate, ' +
+    '.goog-te-banner-frame, ' +
+    '#goog-gt-tt, ' +
+    '.goog-te-balloon-frame, ' +
+    '.goog-te-gadget-simple, ' +
+    '.goog-te-gadget-icon, ' +
+    'iframe[src*="translate.googleapis.com"], ' +
+    '.goog-te-spinner-pos, ' +
+    '#google_translate_element { ' +
+      'display: none !important; ' +
+    '} ' +
+    'body { top: 0px !important; } ' +
+    /* Ẩn thanh thông báo "This page is in English" */
+    '.skiptranslate > iframe:first-child { display: none !important; } ' +
+    'iframe.goog-te-banner-frame { display: none !important; }';
+  document.head.appendChild(style);
+
+  // ---- 1. Tạo widget ẩn + load Google Translate ----
+  function initGT() {
     var div = document.createElement('div');
     div.id = 'google_translate_element';
-    div.style.cssText = 'display:none;';
+    div.style.cssText = 'display:none;position:fixed;top:-9999px;left:-9999px;';
     document.body.appendChild(div);
 
-    // CSS ẩn banner Google
-    var style = document.createElement('style');
-    style.textContent = 
-      '.goog-te-banner-frame.skiptranslate { display:none !important; } ' +
-      'body { top:0px !important; } ' +
-      '.goog-te-gadget-icon { display:none !important; }';
-    document.head.appendChild(style);
-
-    // Hàm init Google Translate
     window.googleTranslateElementInit = function() {
       new google.translate.TranslateElement({
         pageLanguage: 'en',
@@ -50,7 +59,6 @@
       }, 'google_translate_element');
     };
 
-    // Load script Google Translate
     var script = document.createElement('script');
     script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     document.body.appendChild(script);
@@ -61,16 +69,15 @@
     try { localStorage.setItem('gt_lang', lang); } catch(e) {}
 
     if (lang === 'vi') {
-      // Bật Google Translate: set cookie
       document.cookie = 'googtrans=/en/vi; path=/;';
     } else {
-      // Tắt Google Translate: XÓA cookie
+      // Xóa cookie để tắt Google Translate hoàn toàn
       document.cookie = 'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
     }
     location.reload();
   }
 
-  // ---- 3. Gắn sự kiện click cho dropdown ENG/VIE ----
+  // ---- 3. Gắn sự kiện cho dropdown ENG/VIE ----
   document.addEventListener('click', function(e) {
     var link = e.target.closest('.secondary-nav li a');
     if (!link) return;
@@ -84,7 +91,7 @@
     }
   });
 
-  // ---- 4. Đánh dấu active trên dropdown ----
+  // ---- 4. Đánh dấu active ----
   function markActive() {
     var links = document.querySelectorAll('.secondary-nav li a');
     for (var i = 0; i < links.length; i++) {
@@ -99,11 +106,11 @@
   // Boot
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', function() {
-      init();
+      initGT();
       markActive();
     });
   } else {
-    init();
+    initGT();
     markActive();
   }
 })();
